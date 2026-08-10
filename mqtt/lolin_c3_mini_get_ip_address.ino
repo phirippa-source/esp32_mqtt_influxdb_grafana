97eb1cbb08941867dcf3fbd73f0e33b511b987e4
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-08-10 12:47:53 +0900

    Add Wi-Fi connection functionality to get IP address
    This file connects to a specified Wi-Fi network and prints the IP address and RSSI value upon successful connection.

---
+++ mqtt/lolin_c3_mini_get_ip_address.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+const char* WIFI_SSID = "RiaSummer2G";
+const char* WIFI_PASSWORD = "730124go";
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.println("Wi-Fi 연결 테스트");
+
+    // 먼저 Wi-Fi 연결을 시작한다.
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    // WiFi.setTxPower(WIFI_POWER_8_5dBm);   // LOLIN C3 Mini V1.0.0 권장 설정
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nWi-Fi 연결 성공");
+    Serial.print("IP address : ");
+    Serial.println(WiFi.localIP());
+
+    Serial.print("RSSI       : ");
+    Serial.print(WiFi.RSSI());
+    Serial.println(" dBm");
+}
+
+
+void loop() {
+}
+
