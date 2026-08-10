27959e4ad69afa71932937ebc36effa4b6404128
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-08-10 14:00:41 +0900

    Create lolin_c3_mini_sub_msg.ino

---
+++ mqtt/lolin_c3_mini_sub_msg.ino
@@ -0,0 +1,56 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "RiaSummer2G";
+const char* password = "730124go";
+const char* brokerAddress = "192.168.2.8";
+const char* brokerUser = "ship";
+const char* brokerPassword = "1234";
+const char* clientId = "ria_lolin_c3_mini_sub_01";
+const char* topic = "Riatech/A/Line1/Led";
+
+void onMessage(char* topic, byte* payload, unsigned int length) {
+    String message = "";
+    for (int i = 0; i < length; i++) {
+        message += (char)payload[i];
+    }
+    Serial.println("Topic: " + String(topic));
+    Serial.println("Message: " + message);
+}
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(brokerAddress, 1883, onMessage, wifiClient);
+
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.println("Connecting to Wi-Fi: " + String(ssid));
+
+    WiFi.begin(ssid, password);
+    WiFi.setTxPower(WIFI_POWER_8_5dBm);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println();
+    Serial.println("Wi-Fi connected");
+    Serial.println("Connecting to MQTT Broker: " + String(brokerAddress));
+
+    while (!mqttClient.connect(clientId, brokerUser, brokerPassword)) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println();
+    Serial.println("MQTT Broker connected");
+    mqttClient.subscribe(topic);
+    Serial.println("Subscribed topic: " + String(topic));
+}
+
+
+void loop() {
+    mqttClient.loop();
+}
