3fd169aaee1c83d7ff7e7d48ab18ac7a06e53451
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-08-10 11:37:42 +0900

    Create subscriber_id.py

---
+++ mqtt/subscriber_id.py
@@ -0,0 +1,18 @@
+  GNU nano 7.2                        subscriber_id.py                                  import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flag, reason_code, properties):
+    print('Connect with result code : ' + str(reason_code))
+    client.subscribe('Riatech/Factory_A/+/Lux')
+
+def on_message(client, userdata, msg):
+    print(msg.topic + ':' + str(msg.payload))
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+# ID(username)와 PASSWORD(password)를 지정
+client.username_pw_set(username='ship', password='1234')
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect('localhost', 1883, 60)
+client.loop_forever()
