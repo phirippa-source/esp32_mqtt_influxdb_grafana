6f9dc24b55c22ef967bb1d6af59f72bb41d3e752
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-08-12 12:36:51 +0900

    Update InfluxDB connection parameters and formatting

--- influxdb/insert_random_temp.py
+++ influxdb/insert_random_temp.py
@@ -2,29 +2,31 @@ import requests
 import random
 import time
 
-url = "http://YOUR_SERVER_IP_ADDRESS/api/v3/write_lp"
-database = "YOUR_DATABASE_NAME"
-token = "YOUR_TOKEN"  
+url = "http://192.168.2.8:8181/api/v3/write_lp"
+database = "dbtest"
+token = "apiv3_xicSuKg7aBprci2PnxRFgKwXD0fRh995RhL78U-voI6cukfqCV4zNZJ1Ns6zDaPQd8GjePqJZy4Kqxqcz3hVuA"
+
 headers = {
-    "Authorization": f"Bearer {token}"
+    "Authorization": f"Bearer {token}"
 }
 
 params = {
-    "db": database
+    "db": database
 }
 
 while True:
-  temperature = random.uniform(20.0, 30.0)
-  line = f"temperature,location=living_room value={temperature:.1f}"
-  response = requests.post(
-        url,
-        params=params,
-        headers=headers,
-        data=line
-    )
-    if response.status_code == 204:
-        print(line)
-    else:
-        print("Write failed:", response.status_code, response.text)
+    temperature = random.uniform(20.0, 30.0)
+    line = f"temperature,location=living_room value={temperature:.1f}"
+    response = requests.post(
+        url,
+        params=params,
+        headers=headers,
+        data=line 
+    )
 
-    time.sleep(2)
+    if response.status_code == 204:
+        print(line)
+    else:
+        print("Write failed:", response.status_code, response.text)
+
+    time.sleep(2)
