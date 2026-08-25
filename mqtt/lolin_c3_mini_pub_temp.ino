685718085e5baeb10004be271a80b0a72fe0c015
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-08-25 15:42:04 +0900

    Update lolin_c3_mini_pub_temp.ino

--- mqtt/lolin_c3_mini_pub_temp.ino
+++ mqtt/lolin_c3_mini_pub_temp.ino
@@ -3,16 +3,17 @@
 
 const char* ssid = "RiaSummer2G";
 const char* password = "730124go";
-const char* serverIPAddr = "test.mosquitto.org";
+const char* serverIPAddr = "broker.emqx.io";
 
-const char* clientId = "ria_lolin_c3_mini_01";
-const char * topic = "Riatech/Factory_A/Line1/Temp";
+const char* clientId = "ria_lolin_c3_mini_xxxx";
+const char * topic = "Riatech/Linexxxx/Temp";
 
 WiFiClient wifiClient; 
 PubSubClient client(serverIPAddr, 1883, wifiClient);
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
     Serial.print("\nConnecting to " + String(ssid));
     WiFi.begin(ssid, password);         // 지정한 공유기에 접속 시도
     WiFi.setTxPower(WIFI_POWER_8_5dBm); 
